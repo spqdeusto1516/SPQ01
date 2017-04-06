5e36f75ceafa3b5e0c51038cd08487b6ab79399e
Mariabm7 <maria.barredo@opendeusto.es>
2017-04-06 17:15:43 +0200

    .jar

--- Server/BookShopServer/src/bookShopSystem/Review.java
+++ Server/BookShopServer/src/bookShopSystem/Review.java
@@ -13,54 +13,46 @@ public class Review {
 	private User user;
 	private Book book;
 	private String comment;
-	private float rating; 
+	private float rating;
 	
-	public Review(int id_review, User user, Book book, String comment, float rating){
+	public Review(int id_review, User user, Book book, String comment, float rating) {
+		super();
 		this.id_review = id_review;
 		this.user = user;
 		this.book = book;
 		this.comment = comment;
 		this.rating = rating;
 	}
-
+	
 	public int getId_review() {
 		return id_review;
 	}
-	
 	public void setId_review(int id_review) {
 		this.id_review = id_review;
 	}
-	
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
 	public Book getBook() {
 		return book;
 	}
-
 	public void setBook(Book book) {
 		this.book = book;
 	}
 	public String getComment() {
 		return comment;
 	}
-
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-	
 	public float getRating() {
 		return rating;
 	}
-	
 	public void setRating(float rating) {
 		this.rating = rating;
-	}
-	
-	
+	} 
+
 }
