27ba24b3bf1947e36ccab27253a1ac8c1afd577b
Mariabm7 <maria.barredo@opendeusto.es>
2017-04-07 17:51:25 +0200

    jhfyk

--- BookShop/BookShop.sql
+++ BookShop/BookShop.sql
@@ -1,5 +1,5 @@
 DROP SCHEMA  IF EXIXSTS bookshop;
 DROP USER IF EXISTS 'spq'@'%';
 CREATE SCHEMA bookshop;
-CREATE USER IF NOT EXISTS 'spq'@'% IDENTIFIED BY 'spq';
+CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
 GRANT ALL ON bookshop.* TO 'spq'@'%';
