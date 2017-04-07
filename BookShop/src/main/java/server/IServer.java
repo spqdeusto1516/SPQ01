816996d1843ae48e0b5ea38a4fbb5b1b0aa8e604
pablocabezali <paucabla@opendeusto.es>
2017-04-07 12:58:22 +0200

    ugv

---
+++ BookShop/src/main/java/server/IServer.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IServer extends Remote {
+	
+	//String sayMessage(String login, String password, String message) throws RemoteException;
+	//void registerUser(String login, String password) throws RemoteException;
+
+}
