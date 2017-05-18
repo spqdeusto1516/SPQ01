ebd66d9e68dbc53ea32e1c932b0019fda69fa33b
Joel Morisset <jomodeper@opendeusto.es>
2017-05-18 19:42:41 +0200

    javadocs in IDAO and IDB

--- BookShop/src/main/java/db/IDAO.java
+++ BookShop/src/main/java/db/IDAO.java
@@ -5,29 +5,101 @@ import java.util.List;
 
 public interface IDAO {
 
+	/**
+	 * Store a user in the dataBase
+	 * @param u User you want to store
+	 * @return true of false to tell if it worked
+	 */
 	boolean storeUser(User u);
+	
+	/**
+	 * Get a User form the database by its email
+	 * @param email Email of the user you want to get
+	 * @return The User
+	 */
 	User retrieveUser(String email);
+	
+	/**
+	 * Updates the row in the database of a given User 
+	 * @param u User you are updating
+	 * @return true of false to tell if it worked
+	 */
 	boolean updateUser(User u);
 
 	
+	/**
+	 * Get a Review from the database by its ID
+	 * @param id_review
+	 * @return The Review
+	 */
 	Review retrieveReview(int id_review );
+	
+	/**
+	 * Updates the row in the database of a given Review
+	 * @param r the review to update
+	 * @return true of false to tell if it worked
+	 */
 	boolean updateReview(Review r);
 
+	/**
+	 * Store a Book in the database
+	 * @param b the Book you want to store
+	 * @return true of false to tell if it worked
+	 */
 	boolean storeBook(Book b);
+	
+	/**
+	 * Get a Book from the database by its ISBN
+	 * @param ISBN
+	 * @return The Book
+	 */
 	Book retrieveBook(int ISBN );
+	
+	/**
+	 * Updates the row in the database of a given Book
+	 * @param b the book
+	 * @return true of false to tell if it worked
+	 */
 	boolean updateBook(Book b);
 
+	/**
+	 * Get a Book from the database by its title
+	 * @param title of the book
+	 * @return The book
+	 */
 	Book retrieveBookByParameter(String title);
 	
 	// boolean deleteDatabase();
 
 	//Get all entities
 
+	/**
+	 * get all the Users of the Database
+	 * @return A List<User> of all users 
+	 */
 	public List<User> getAllUsers();
+	
+	/**
+	 * get all the Reviews of the Database
+	 * @return A List<Review> of all users 
+	 */
 	public List<Review> getAllReviews();
+	
+	/**
+	 * get all the Books of the Database
+	 * @return A List<Book> of all users 
+	 */
 	public List<Book> getAllBooks();
 	
+	/**
+	 * Eliminates a Review from the database
+	 * @param r the review to eliminate
+	 */
 	void deleteReview(Review r);
+	/**
+	 * Eliminates a book from the database
+	 * @param b the book to eliminate
+	 */
 	void deleteBook(Book b);
 
 
--- BookShop/src/main/java/db/IDB.java
+++ BookShop/src/main/java/db/IDB.java
@@ -1,34 +1,135 @@
 package db;
 
 
-	import java.util.List;
+	import java.rmi.RemoteException;
+import java.util.List;
 
 	import server.data.*;
 
 	public interface IDB {
 		
-		  List<User> getAllUsers();
-		  List<Review> getAllReviews();
-		  List<Book> getAllBooks();
+		/**
+		 * get all the Users of the Database
+		 * @return A List<User> of all users 
+		 */
+		List<User> getAllUsers();
 		
-		  boolean buyBook(String email, String book_title);
+		/**
+		 * get all the Reviews of the Database
+		 * @return A List<Review> of all users 
+		 */
+		List<Review> getAllReviews();
+		
+		/**
+		 * get all the Books of the Database
+		 * @return A List<Book> of all users 
+		 */
+		List<Book> getAllBooks();
+		
+		  /**
+		   * Allow the User to buy a selected book
+		 * @param email Email of the buying user
+		 * @param book_title the title of the book to buy
+		 * @return true or false to tell if it worked
+		 */
+		boolean buyBook(String email, String book_title);
 	
-			boolean registerUser(String email, String password,boolean role);
-	
-			boolean addBookToDb(Book b);
-			boolean addReview(Book b, Review r, User user );
-			
-			Book showBookByISBN	(int ISBN);
-			Book showBookByTitle	(String title);
-			Review showReview	(int id_review);
-			User showUser	(String email);
-			List<Review> getUserReviews(String email);
-			List<Review> getBookReviews(String title);
-			double averageRatingByBook(String title);
-			double averageRatingByUser(String email);
-			boolean deleteReview(int id_review);
-			boolean deleteBook(int ISBN);
-			
+		/**
+		  * creates a new user
+		 * @param email 
+		 * @param password
+		 * @param role (true admin; false user)
+		 * @return true or false to tell if it worked
+		 */
+		boolean registerUser(String email, String password,boolean role);
+
+		/**
+		 * Adds a new book to the database
+		 * @param b the book to add
+		 * @return true or false to tell if it worked
+		 */
+		boolean addBookToDb(Book b);
+		
+		/**
+		 * Adds a new Review to the database
+		 * @param b the book its from
+		 * @param r the review
+		 * @param user the user that wrote it
+		 * @return true or false to tell if it worked
+		 */
+		boolean addReview(Book b, Review r, User user );
+
+		/**
+		 * Get a book from  the database by its ISBN
+		 * @param ISBN
+		 * @return The Book 
+		 */
+		Book showBookByISBN	(int ISBN);
+		
+		/**
+		 * Get a Book from the database by its title
+		 * @param title of the book
+		 * @return The book
+		 */
+		Book showBookByTitle	(String title);
+		
+		/**
+		 * Get a Review from the database by its ID
+		 * @param id_review
+		 * @return The Review
+		 */
+		Review showReview	(int id_review);
+		
+		/**
+		 * Get a User form the database by its email
+		 * @param email Email of the user you want to get
+		 * @return The User
+		 */
+		User showUser	(String email);
+		
+		/**
+		 * Get all the reviews a User wrote 
+		 * @param email of the user
+		 * @return A List<Review> of a given user
+		 */
+		List<Review> getUserReviews(String email);
+		
+		/**
+		 * Get all the reviews of a book
+		 * @param title of the book
+		 * @return A List<Review> of a given book
+		 */
+		List<Review> getBookReviews(String title);
+		
+		/**
+		 * Gets the average rating of a book
+		 * @param title of the book
+		 * @return a double the average rating
+		 */
+		double averageRatingByBook(String title);
+		
+		/**
+		 * Gets the average rating given by a user
+		 * @param email of the user
+		 * @return a double the average rating
+		 */
+		double averageRatingByUser(String email);
+		
+		/**
+		 * Eliminates a Review from the database
+		 * @param id_review 
+		 * @return true or false to tell if it worked
+		 */
+		boolean deleteReview(int id_review);
+		
+		/**
+		 * Eliminates a book from the database
+		 * @param ISBN
+		 * @return true or false to tell if it worked
+		 */
+		boolean deleteBook(int ISBN);
+		
+
 		
 		
 }
